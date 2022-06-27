8580858a334a7458edbf361f6b15ea9391e6e5f1
osamasabry9 <osamasabry324@gmail.com>
2022-06-27 06:30:01 +0200

    shared Files

---
+++ lib/shared/config/app_colors.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+static const backgroundColor = Color.fromRGBO(32, 33, 36, 1);
+static const searchColor = Color.fromRGBO(48, 49, 52, 1);
+static const blueColor = Color.fromRGBO(138, 180, 248, 1);
+static const searchBorder = Color.fromRGBO(95, 99, 104, 1);
+static const primaryColor = Colors.white;
+static const footerColor = Color.fromRGBO(23, 23, 23, 1);
+}
