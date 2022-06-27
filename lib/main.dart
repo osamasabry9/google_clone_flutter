ef079576e70eeadff86f2221a73577094eca70f0
osamasabry9 <osamasabry324@gmail.com>
2022-06-27 06:31:00 +0200

    responsive layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_clone/views/pages/mobile_screen_layout.dart';
+import 'package:google_clone/layouts/responsive_layout_screen.dart';
+import 'package:google_clone/views/pages/web_screen_layout.dart';
+import 'package:google_clone/shared/config/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: 'Google Clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: AppColors.backgroundColor),
+      home: const ResponsiveLayoutScreen(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
       ),
-      home: const Scaffold(),
     );
   }
 }
---
+++ lib/views/pages/web_screen_layout.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_clone/shared/config/app_colors.dart';
+import 'package:google_clone/views/widgets/search.dart';
+import 'package:google_clone/views/widgets/web/search_buttons.dart';
+import 'package:google_clone/views/widgets/web/translation_buttons.dart';
+
+class WebScreenLayout extends StatelessWidget {
+  const WebScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: AppColors.backgroundColor,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Gmail',
+              style: TextStyle(
+                color: AppColors.primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Images',
+              style: TextStyle(
+                color: AppColors.primaryColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              'assets/images/more-apps.svg',
+              color: AppColors.primaryColor,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: MaterialButton(
+              color: const Color(0xff1A73EB),
+              onPressed: () {},
+              child: const Text(
+                'Sign in',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: _size.height * 0.18,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: const [
+                      Search(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      SearchButtons(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TranslationButtons(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
